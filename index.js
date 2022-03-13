95fb20aa681eec87e0d1bfa6c807798510607f1c
Adrian Valdes <adrianvaldeshdz@gmail.com>
2022-03-13 11:07:42 +0100

    add possibility of having different kind of hooks

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-function useState(initValue) {
-    let _value = initValue
-    const state = () => _value
-    const setState = newValue => {
-        _value = newValue
-    }
-    return [state, setState]
+
+const React = (() => {
+  let hooks = [];
+  let idx = 0
+  function useState(initValue) {
+    const state = hooks[idx] || initValue;
+    const _idx = idx
+    const setState = (newValue) => {
+      hooks[_idx] = newValue;
+    };
+    idx++
+    return [state, setState];
+  }
+  function render(Component) {
+      idx = 0
+      const C = Component()
+      C.render()
+      return C
+  }
+
+  return { useState, render };
+})();
+
+function Component() {
+  const [count, setCount] = React.useState(1);
+  const [text, setText] = React.useState("book");
+  return {
+    render: () => console.log({count, text}),
+    click: () => setCount(count + 1),
+    type: (word) => setText(word)
+  };
 }
 
-const [count, setCount] = useState(1)
+let App = React.render(Component)
+App.click()
+App = React.render(Component)
+App.type("magazine")
+App = React.render(Component)
 
-console.log(count())
-setCount(2)
-console.log(count())
+
+
+
